e6ecf49329126cfdf34e8fb7c592a78a390a2ffd
assignment-01 <ykhan9463@gmail.com>
2025-09-04 09:53:38 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-10/12 mx-auto mt-3">
+        <h2 className="text-xl bg-amber-300">BPL-DREAM 11</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
